ca5ef81d32ab1be2f8980cf5920f8eec4b5f442f
Nayem Uddin <nurakib.nstu@gmail.com>
2016-04-02 19:12:41 +0600

    very easy

--- UVa/uva_11530.c
+++ UVa/uva_11530.c
@@ -10,11 +10,11 @@ int main(){
         gets(ch);
         len = strlen(ch);
         for(i = 0; i < len; i++){
-            if(ch[i] == 'a' || ch[i] == 'd' || ch[i] == 'g' || ch[i] == 'j' || ch[i] == 'm' || ch[i] == 'p' || ch[i] == 't' ||               ch[i] == 'w' || ch[i] == ' ')
+            if(ch[i] == 'a' || ch[i] == 'd' || ch[i] == 'g' || ch[i] == 'j' || ch[i] == 'm' || ch[i] == 'p' || ch[i] == 't' || ch[i] == 'w' || ch[i] == ' ')
                 ans += 1;
-            else if(ch[i] == 'b' || ch[i] == 'e' || ch[i] == 'h' || ch[i] == 'k' || ch[i] == 'n' || ch[i] == 'q' || ch[i] == 'u'             || ch[i] == 'x')
+            else if(ch[i] == 'b' || ch[i] == 'e' || ch[i] == 'h' || ch[i] == 'k' || ch[i] == 'n' || ch[i] == 'q' || ch[i] == 'u' || ch[i] == 'x')
                 ans += 2;
-            else if(ch[i] == 'c' || ch[i] == 'f' || ch[i] == 'i' || ch[i] == 'l' || ch[i] == 'o' || ch[i] == 'r' || ch[i] == 'v'             || ch[i] == 'y')
+            else if(ch[i] == 'c' || ch[i] == 'f' || ch[i] == 'i' || ch[i] == 'l' || ch[i] == 'o' || ch[i] == 'r' || ch[i] == 'v' || ch[i] == 'y')
                 ans += 3;
             else if(ch[i] == 's' || ch[i] == 'z')
                 ans += 4;
