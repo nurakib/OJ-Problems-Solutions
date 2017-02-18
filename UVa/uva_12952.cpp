b39153a4289e2854b93584944fc684cb30c1b4b3
Nayem Uddin <nurakib.nstu@gmail.com>
2017-02-18 21:22:11 +0600

    very easy

---
+++ UVa/uva_12952.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    while(cin >> a >> b){
+        if(a >= b) cout << a << endl;
+        else cout << b << endl;
+    }
+    return 0;
+}
