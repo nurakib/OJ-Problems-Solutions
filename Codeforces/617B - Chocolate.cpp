bb6d23465bde9271bf560a590a0624721a288edb
Nayem Uddin <nurakib.nstu@gmail.com>
2017-09-01 10:45:55 +0600

    combi

---
+++ Codeforces/617B - Chocolate.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, v;
+    cin >> n;
+    int prev = -1;
+    long long result = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> v;
+        if(v == 1){
+            if (prev == -1) result = 1;
+            else result *= i - prev;
+            prev = i;
+        }
+    }
+    cout << result << endl;
+}
